1ffea610b570a41526ec330a86d0a0e620762412
raaarwwwwandy <hirandysan@gmail.com>
2016-12-01 20:05:47 -1000

    created a new spell

--- constructors.js
+++ constructors.js
@@ -11,6 +11,12 @@
  * @method   getDetails
  */
 
+ function Spell(name, cost, description){
+      this.name = "catogato";
+      this.cost = 50;
+      this.description = "creates a cat out of no where";
+ }
+
   /**
    * Returns a string of all of the spell's details.
    * The format doesn't matter, as long as it contains the spell name, cost, and description.
@@ -18,6 +24,12 @@
    * @name getDetails
    * @return {string} details containing all of the spells information.
    */
+
+   Spell.prototype.getDetails = function() {
+      let details = [name, cost, description];
+      return details;
+   };
+
 
 /**
  * A spell that deals damage.
